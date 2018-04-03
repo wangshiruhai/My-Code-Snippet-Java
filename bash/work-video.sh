c0f93883210fb8a5d7a00fb08d4f9006022e1152
wangshihai <wangshihai130@163.com>
2018-04-03 17:26:04 +0800

    Delete work-video.sh
    删除视频加密脚本文件

--- bash/work-video.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-BASE_URL=${1:-'.'}
-openssl rand 16 > file.key
-echo $BASE_URL/file.key > file.keyinfo
-echo file.key >> file.keyinfo
-echo $(openssl rand -hex 16) >> file.keyinfo
-#ffmpeg -f lavfi -re -i testsrc -c:v h264 -hls_flags delete_segments \
-#  -hls_key_info_file file.keyinfo out.m3u8
